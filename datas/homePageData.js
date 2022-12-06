0ac1e148fd98d3b980296f18df1e64c2a43d0bca
Egy <91923127+Enkhmunkhz@users.noreply.github.com>
2022-12-06 09:16:00 +0800

    change photo urgutuggul

--- datas/homePageData.js
+++ datas/homePageData.js
@@ -4,48 +4,48 @@ export const data = [
     movies: [
       {
         id: 1,
-        image: "../images/movie1.jpg",
+        image: "../images/movie1.webp",
         type: ["ГЭМТ ХЭРЭГ", "УЯНГА"],
       },
       {
         id: 2,
-        image: "../images/movie2.jpg",
+        image: "../images/movie2.webp",
         type: ["АДАЛ ЯВДАЛ", "УЯНГА"],
       },
       {
         id: 3,
-        image: "../images/movie3.jpg",
+        image: "../images/movie3.webp",
         type: ["ГЭМТ ХЭРЭГ", "УЯНГА", "ГЭР БҮЛ"],
       },
       {
         id: 4,
-        image: "../images/movie4.jpg",
+        image: "../images/movie4.webp",
         type: ["ИНЭЭДЭМ", "УЯНГА"],
       },
       {
         id: 5,
-        image: "../images/movie5.jpg",
+        image: "../images/movie5.webp",
         type: ["ИНЭЭДЭМ", "ГЭР БҮЛ"],
       },
       {
         id: 111,
-        image: "../images/movie1.jpg",
+        image: "../images/movie1.webp",
       },
       {
         id: 211,
-        image: "../images/movie2.jpg",
+        image: "../images/movie2.webp",
       },
       {
         id: 311,
-        image: "../images/movie3.jpg",
+        image: "../images/movie3.webp",
       },
       {
         id: 411,
-        image: "../images/movie4.jpg",
+        image: "../images/movie4.webp",
       },
       {
         id: 511,
-        image: "../images/movie5.jpg",
+        image: "../images/movie5.webp",
       },
     ],
   },
@@ -54,24 +54,24 @@ export const data = [
     movies: [
       {
         id: 6,
-        image: "../images/movie6.jpg",
+        image: "../images/movie6.webp",
         type: ["ИНЭЭДЭМ", "РОМАНТИК"],
       },
       {
         id: 7,
-        image: "../images/movie7.jpg",
+        image: "../images/movie7.webp",
       },
       {
         id: 8,
-        image: "../images/movie8.jpg",
+        image: "../images/movie8.webp",
       },
       {
         id: 9,
-        image: "../images/movie9.jpg",
+        image: "../images/movie9.webp",
       },
       {
         id: 10,
-        image: "../images/movie10.jpg",
+        image: "../images/movie10.webp",
       },
     ],
   },
@@ -80,23 +80,23 @@ export const data = [
     movies: [
       {
         id: 11,
-        image: "../images/movie11.jpg",
+        image: "../images/movie11.webp",
       },
       {
         id: 12,
-        image: "../images/movie12.jpg",
+        image: "../images/movie12.webp",
       },
       {
         id: 13,
-        image: "../images/movie13.jpg",
+        image: "../images/movie13.webp",
       },
       {
         id: 14,
-        image: "../images/movie14.jpg",
+        image: "../images/movie14.webp",
       },
       {
         id: 15,
-        image: "../images/movie15.jpg",
+        image: "../images/movie15.webp",
       },
     ],
   },
@@ -105,23 +105,23 @@ export const data = [
     movies: [
       {
         id: 16,
-        image: "../images/movie16.jpg",
+        image: "../images/movie16.webp",
       },
       {
         id: 17,
-        image: "../images/movie17.jpg",
+        image: "../images/movie17.webp",
       },
       {
         id: 18,
-        image: "../images/movie18.jpg",
+        image: "../images/movie18.webp",
       },
       {
         id: 19,
-        image: "../images/movie19.jpg",
+        image: "../images/movie19.webp",
       },
       {
         id: 20,
-        image: "../images/movie20.jpg",
+        image: "../images/movie20.webp",
       },
     ],
   },
